7e1451b89a3e167a163417c0ccd322c11d6b4fa9
Ram Mohan <chandu270>
2021-01-22 17:55:58 +0530

    Todolist-v1

---
+++ app.js
@@ -0,0 +1,60 @@
+//jshint esversion:6
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const date = require( __dirname + "/date.js");
+
+const items = ["Buy food","Cook food","Eat food"];
+
+const workItems=[];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+const day = date.getDate();
+
+res.render("list",{listTitle: day,newListItems: items});
+
+app.post("/", function(req,res){
+   let item = req.body.newItem;
+   if(req.body.list ==="Work"){
+       workItems.push(item);
+       res.redirect("/work");
+   }else{
+    items.push(item);
+    res.redirect("/");
+   }
+  
+});
+
+app.get("/work",function(req,res){
+res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+  });
+    
+// app.post("/work",function(req,res){
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// });
+
+
+
+
+
+
+
+
+
